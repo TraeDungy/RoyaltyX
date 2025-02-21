3387669fb422d32faf75adbb94e932c319317c85
Nenad Blagov <nbl@nexyo.ag>
2025-02-21 01:31:10 +0100

    updates

--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -1,9 +1,11 @@
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Product
 from .serializers import ProductSerializer
+
 
 # List all products or create a new product
 @api_view(['GET', 'POST'])
