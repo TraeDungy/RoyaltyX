3f6ae57e5287edd8995dc8824c717e476c7cbe08
Nenad Blagov <nbl@nexyo.ag>
2025-03-22 00:25:03 +0100

    ruff fix

--- backend/apps/inbox/views.py
+++ backend/apps/inbox/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Conversation, Message
+from .models import Conversation
 from .serializers import ConversationSerializer, MessageSerializer
 
 
