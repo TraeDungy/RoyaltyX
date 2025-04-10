52208ef6e7ac0ef203b6ee2dfe71ad47bae7ad70
Nenad Blagovcanin <nbl@nexyo.ag>
2025-04-10 02:12:30 +0200

    updates

--- frontend/src/modules/dashboard/pages/Dashboard.jsx
+++ frontend/src/modules/dashboard/pages/Dashboard.jsx
@@ -27,7 +27,7 @@ function Dashboard() {
       <div className="row">
         <div className="col-md-3 pb-4">
           <div
-            className="card border-0"
+            className="card border-0 h-100"
             style={{
               background:
                 "linear-gradient(135deg,rgba(107, 17, 203, 0.50),rgba(37, 116, 252, 0.5))",
@@ -37,7 +37,7 @@ function Dashboard() {
             <div className="card-body d-flex flex-column justify-content-between">
               <div className="">
                 <Search className="h3 mb-3" />
-                <h6>Smart search</h6>
+                <h5>Smart search</h5>
                 <p className="medium">
                   Use advanced algorithms to find specific information.
                 </p>
@@ -47,7 +47,7 @@ function Dashboard() {
         </div>
         <div className="col-md-3 pb-4">
           <div
-            className="card border-0"
+            className="card border-0 h-100"
             style={{
               background:
                 "linear-gradient(135deg,rgba(255, 126, 180, 0.5),rgba(255, 117, 140, 0.5))",
@@ -57,7 +57,7 @@ function Dashboard() {
             <div className="card-body d-flex flex-column justify-content-between">
               <div className="">
                 <Git className="h3 mb-3" />
-                <h6>Updated reporting</h6>
+                <h5>Updated reporting</h5>
                 <p className="medium">
                   Keep track of versions to ensure data integrity.
                 </p>
@@ -67,7 +67,7 @@ function Dashboard() {
         </div>
         <div className="col-md-3 pb-4">
           <div
-            className="card border-0"
+            className="card border-0 h-100"
             style={{
               background:
                 "linear-gradient(135deg,rgba(17, 153, 142, 0.5),rgba(56, 239, 126, 0.5))",
@@ -77,7 +77,7 @@ function Dashboard() {
             <div className="card-body d-flex flex-column justify-content-between">
               <div className="">
                 <GraphUpArrow className="h3 mb-3" />
-                <h6>Custom analytics</h6>
+                <h5>Custom analytics</h5>
                 <p className="medium">
                   Access your notes across all your work apps.
                 </p>
@@ -87,7 +87,7 @@ function Dashboard() {
         </div>
         <div className="col-md-3 pb-4">
           <div
-            className="card border-0"
+            className="card border-0 h-100"
             style={{
               background:
                 "linear-gradient(135deg,rgba(255, 65, 109, 0.5),rgba(255, 75, 43, 0.5))",
@@ -97,9 +97,9 @@ function Dashboard() {
             <div className="card-body d-flex flex-column justify-content-between">
               <div className="">
                 <Envelope className="h3 mb-3" />
-                <h6>Email and share reports</h6>
+                <h5>Generate reports</h5>
                 <p className="medium">
-                  Embed images, links and files to create more dynamic notes.
+                  Embed images, links & files to create notes.
                 </p>
               </div>
             </div>
@@ -107,7 +107,7 @@ function Dashboard() {
         </div>
         <div className="col-md-6 pb-4">
           <div
-            className="card border-0"
+            className="card border-0 h-100"
             style={{
               background:
                 "linear-gradient(135deg,rgba(252, 75, 26, 0.5),rgba(247, 182, 51, 0.5))",
@@ -117,7 +117,7 @@ function Dashboard() {
             <div className="card-body d-flex flex-column justify-content-between">
               <div className="">
                 <Bell className="h3 mb-3" />
-                <h6>Reminders & alerts</h6>
+                <h5>Reminders & alerts</h5>
                 <p className="medium">
                   Sets reminders and alerts for upcoming deadlines or tasks.
                 </p>
@@ -127,7 +127,7 @@ function Dashboard() {
         </div>
         <div className="col-md-6 pb-4">
           <div
-            className="card border-0"
+            className="card border-0 h-100"
             style={{
               background:
                 "linear-gradient(135deg,rgba(5, 118, 230, 0.5),rgba(0, 242, 97, 0.5))",
@@ -137,7 +137,7 @@ function Dashboard() {
             <div className="card-body d-flex flex-column justify-content-between">
               <div className="">
                 <ShieldCheck className="h3 mb-3" />
-                <h6>Encryption</h6>
+                <h5>Encryption</h5>
                 <p className="medium">
                   Implements end-to-end encryption for robust security.
                 </p>
