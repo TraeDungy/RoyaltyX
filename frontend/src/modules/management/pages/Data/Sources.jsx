861a1e3590427ec49945867ce5398a718b21a96e
NENAD-BLAGOVCANIN <nenadblagovcanin2@gmail.com>
2025-06-20 17:47:44 +0200

    added platform logos to local folder

--- frontend/src/modules/management/pages/Data/Sources.jsx
+++ frontend/src/modules/management/pages/Data/Sources.jsx
@@ -1,79 +1,134 @@
+import { Box, Typography, Card, Button, Grid } from "@mui/material";
 import PageHeader from "../../../common/components/PageHeader";
+import youtubeLogo from "../../../common/assets/img/platform_logos/youtube.webp";
+import googleAdsLogo from "../../../common/assets/img/platform_logos/google_ads.webp";
+import amazonLogo from "../../../common/assets/img/platform_logos/amazon.webp";
 
 const Sources = () => {
   return (
-    <div className="py-3">
+    <Box sx={{ py: 3 }}>
       <PageHeader
         title="Sources"
         description="Manage your data sources and link your platforms of choice."
       />
 
-      <div className="row mt-4">
-        <div className="col-md-6 pb-4">
-          <div className="card p-3 py-4 rounded shadow-lg power-contrast border-0">
-            <div className="row">
-              <div className="col-md-3 d-flex align-items-center justify-content-center">
+      <Grid container spacing={4} sx={{ mt: 4 }}>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Card sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
+            <Grid container spacing={2}>
+              <Grid
+                item
+                xs={3}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
                 <img
-                  src="https://cdn3.iconfinder.com/data/icons/social-network-30/512/social-06-512.png"
+                  src={youtubeLogo}
                   alt="YouTube Logo"
-                  style={{ height: "80px", objectFit: "contain" }}
+                  style={{ height: "70px", objectFit: "contain" }}
                 />
-              </div>
-              <div className="col-md-9">
-                <h5>Youtube</h5>
-                <p className="txt-lighter medium mb-3">
+              </Grid>
+              <Grid item xs={9}>
+                <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  YouTube
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary", mb: 3 }}
+                >
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
                   laborum a veniam velit iure.
-                </p>
-                <button className="btn btn-black-white">Link YouTube</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6 pb-4">
-          <div className="card p-3 py-4 rounded shadow-lg power-contrast border-0">
-            <div className="row">
-              <div className="col-md-3 d-flex align-items-center justify-content-center">
+                </Typography>
+                <Grid item xs={12}>
+                  <Button variant="outlined" sx={{ mt: 2, display: "block" }}>
+                    Link YouTube
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Card sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
+            <Grid container spacing={2}>
+              <Grid
+                item
+                xs={3}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
                 <img
-                  src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_ads-1024.png"
-                  alt="YouTube Logo"
-                  style={{ height: "80px", objectFit: "contain" }}
+                  src={googleAdsLogo}
+                  alt="Google Ads Logo"
+                  style={{ height: "70px", objectFit: "contain" }}
                 />
-              </div>
-              <div className="col-md-9">
-                <h5>Google Ads</h5>
-                <p className="txt-lighter medium mb-3">
+              </Grid>
+              <Grid item xs={9}>
+                <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  Google Ads
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary", mb: 3 }}
+                >
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
                   laborum a veniam velit iure.
-                </p>
-                <button className="btn btn-black-white">Link Google Ads</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6 pb-4">
-          <div className="card p-3 py-4 rounded shadow-lg power-contrast border-0">
-            <div className="row">
-              <div className="col-md-3 d-flex align-items-center justify-content-center">
+                </Typography>
+                <Grid item xs={12}>
+                  <Button variant="outlined" sx={{ mt: 2, display: "block" }}>
+                    Link Google Ads
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Card sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
+            <Grid container spacing={2}>
+              <Grid
+                item
+                xs={3}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
                 <img
-                  src="https://logo-marque.com/wp-content/uploads/2020/04/Amazon-Embleme.png"
-                  alt="YouTube Logo"
-                  style={{ height: "80px", objectFit: "contain" }}
+                  src={amazonLogo}
+                  alt="Amazon Logo"
+                  style={{ height: "70px", objectFit: "contain" }}
                 />
-              </div>
-              <div className="col-md-9">
-                <h5>Amazon</h5>
-                <p className="txt-lighter medium mb-3">
+              </Grid>
+              <Grid item xs={9}>
+                <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  Amazon
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary", mb: 3 }}
+                >
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
                   laborum a veniam velit iure.
-                </p>
-                <button className="btn btn-black-white">Link Amazon</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                </Typography>
+                <Grid item xs={12}>
+                  <Button variant="outlined" sx={{ mt: 2, display: "block" }}>
+                    Link Amazon
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
